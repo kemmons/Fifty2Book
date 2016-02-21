cb8b3e72bd2b56ecc1ffddd239c46c267249d274
Kelsey Emmons <kelsey.emmons@cerner.com>
2016-02-20 18:17:01 -0600

    Update goals in SettingActivity implemented.

--- app/src/main/java/com/emmons/kelsey/umkc/fifty2book/MainActivity.java
+++ app/src/main/java/com/emmons/kelsey/umkc/fifty2book/MainActivity.java
@@ -1,7 +1,10 @@
 package com.emmons.kelsey.umkc.fifty2book;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +12,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String PREFS_NAME = "PrefsFile";
-
+    private final String GOAL = "goal", READ = "read", REM = "to_read";
     private int read = 0, to_read = 0, goal = 0;
 
 
@@ -30,13 +35,34 @@ public class MainActivity extends AppCompatActivity {
         //toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        LoadPreferences();
+        UpdateGoals();
     }
 
+    private void LoadPreferences() {
+        SharedPreferences goalPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        goal = goalPrefs.getInt(GOAL,0);
+        to_read = goalPrefs.getInt(GOAL, 0) - read;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    private void UpdateGoals() {
+        TextView t = (TextView) findViewById(R.id.goalValue);
+        t.setText(Integer.toString(goal));
+        t = (TextView) findViewById(R.id.remainValue);
+        t.setText(Integer.toString(to_read));
+        if (goal > 0) {
+            int progress = ((to_read / goal) * 100);
+            t = (TextView) findViewById(R.id.progressPercent);
+            t.setText(Integer.toString(progress) + "%");
+            ProgressBar prog = (ProgressBar) findViewById(R.id.goalProgress);
+            prog.setProgress(progress);
+        }
     }
 
     @Override
@@ -56,4 +82,5 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
 }
--- app/src/main/java/com/emmons/kelsey/umkc/fifty2book/SettingsActivity.java
+++ app/src/main/java/com/emmons/kelsey/umkc/fifty2book/SettingsActivity.java
@@ -1,21 +1,44 @@
 package com.emmons.kelsey.umkc.fifty2book;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.security.Key;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "PrefsFile";
+    private final String GOAL = "goal", READ = "read", REM = "to_read";
+
+    private int goal = 0, read = 0, to_read = 0;
+    private int time_left = 0;
+    private String time_type = "none";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setListeners();
+    }
 
+    private void setListeners() {
         Spinner goalSpinner = (Spinner) findViewById(R.id.goalSpinner);
 
         ArrayAdapter<CharSequence> goalSpinAdapter = ArrayAdapter
@@ -26,9 +49,38 @@ public class SettingsActivity extends AppCompatActivity {
 
         goalSpinner.setAdapter(goalSpinAdapter);
 
-        Intent intent = getIntent();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button setGoalButton = (Button) findViewById(R.id.setGoalButton);
+        View.OnClickListener sGButton = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editBookGoal = (EditText) findViewById(R.id.booksGoal);
+                EditText editTimeGoal = (EditText) findViewById(R.id.weeksGoal);
+                Spinner goalSpinner = (Spinner) findViewById(R.id.goalSpinner);
+
+                goal = Integer.parseInt(editBookGoal.getText().toString());
+                time_left = Integer.parseInt(editTimeGoal.getText().toString());
+                time_type = goalSpinner.getSelectedItem().toString();
+
+                SavePreferences();
+            }
+        };
+        setGoalButton.setOnClickListener(sGButton);
+    }
+
+    private void SavePreferences() {
+        SharedPreferences goalPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        SharedPreferences.Editor prefsEditor = goalPrefs.edit();
+        prefsEditor.putInt(GOAL,goal);
+        prefsEditor.putInt(READ,read);
+        prefsEditor.putInt(REM, to_read);
+        prefsEditor.apply();
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
